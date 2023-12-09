e8a8baac3675566c2900810c1d05448689f064ba
JuuFrank <129233754+JuuFrank@users.noreply.github.com>
2023-12-09 02:37:30 -0500

    asd

--- login/src/main/java/login/basic/customer/domain/persistence/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package login.basic.customer.domain.persistence;
-
-import login.basic.customer.domain.model.entities.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Integer> {
-
-    Client findByCorreo(String correo);
-}
